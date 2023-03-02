d25452c8ad8aa318dee1d093e508e86ea05f4b4f
Dan Harling <dharling101@gmail.com>
2023-03-02 11:02:20 +0000

    fix: add cors

--- controllers/tasks.js
+++ controllers/tasks.js
@@ -1,6 +1,6 @@
 const getHeaders = (req, res) => {
 	res.json({
-		ipaddress: req.ip,
+		ipaddress: req.socket.remoteAddress,
 		language: req.get("accept-language"),
 		software: req.get("user-agent"),
 	});
--- index.js
+++ index.js
@@ -1,12 +1,14 @@
 const express = require("express");
 const app = express();
 require("dotenv").config();
+const cors = require("cors");
 
 const router = require("./routes/tasks");
 
 const PORT = process.env.PORT || 8080;
 
 app.use(express.json());
+app.use(cors({ optionsSuccessStatus: 200 }));
 
 app.use("/", router);
 
